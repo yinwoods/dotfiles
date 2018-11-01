8ad6a2db84619718e278d9be6795d654d69ff3ef
ctyin <ctyin@freewheel.tv>
2018-11-02 00:02:25 +0800

    update vim

--- .config/yarn/global/node_modules/@vue/cli-ui/src/util/folders.js
+++
@@ -1,8 +0,0 @@
-export function isValidName (name) {
-  return !name.match(/[/@\s+%:]/) && encodeURIComponent(name) === name
-}
-
-export function isValidMultiName (name) {
-  name = name.replace(/\\/g, '/')
-  return name.split('/').every(isValidName)
-}
